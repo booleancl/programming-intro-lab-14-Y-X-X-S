167c06abcfea7bed12ca09d4df6605a007c096b5
Yariyari <yaritza.urbina.sanhueza@gmail.com>
2022-10-14 10:54:15 -0300

    cardinalidad de relaciones

--- migrations/4_seedData.sql
+++ migrations/4_seedData.sql
@@ -1,1 +1,5 @@
+\c blog
 
+\COPY users(email) FROM 'C:/Users/user/Intro_Dev/programming-intro-lab-14-Y-X-X-S/data/users.csv' csv HEADER;
+\COPY posts FROM 'C:/Users/user/Intro_Dev/programming-intro-lab-14-Y-X-X-S/data/posts.csv' csv HEADER;
+\COPY comments FROM 'C:/Users/user/Intro_Dev/programming-intro-lab-14-Y-X-X-S/data/comentarios.csv' csv HEADER;
